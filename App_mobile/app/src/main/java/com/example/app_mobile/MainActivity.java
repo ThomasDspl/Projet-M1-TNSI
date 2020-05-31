cc550a4286a592956e269fc7735f4bde8e163997
Trehou Thomas <trehou.thomas@outlook.fr>
2020-05-31 11:25:28 +0200

    Nouveau menu

--- App_mobile/app/src/main/java/com/example/app_mobile/MainActivity.java
+++ App_mobile/app/src/main/java/com/example/app_mobile/MainActivity.java
@@ -26,20 +26,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                R.id.nav_accueil, R.id.nav_stats, R.id.nav_ajoutphoto,R.id.nav_carte,R.id.nav_classement,R.id.nav_compte)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
