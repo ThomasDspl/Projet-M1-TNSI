4ea0c8841f9dc857cdc971238346ed9f6775df78
ThomasDspl <thomas.displaire@outlook.fr>
2020-06-06 19:37:19 +0200

    Added lgoic into account fragment

--- AppMobile/app/src/main/java/com/example/appmobile/CompteFragment.java
+++ AppMobile/app/src/main/java/com/example/appmobile/CompteFragment.java
@@ -1,9 +1,12 @@
 package com.example.appmobile;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,6 +18,19 @@ public class CompteFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_compte, container, false);
+        Button inscrire = rootView.findViewById(R.id.btn_inscrire);
+
+        inscrire.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
         return inflater.inflate(R.layout.fragment_compte,container, false);
     }
 }
