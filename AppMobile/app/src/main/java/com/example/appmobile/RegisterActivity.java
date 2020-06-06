6667df1909d74daaf49db34da81d6841e403190a
Soizik <trehou.thoms@outlook.fr>
2020-06-06 17:39:36 +0200

    Onglet Connexion

--- AppMobile/app/src/main/java/com/example/appmobile/RegisterActivity.java
+++ AppMobile/app/src/main/java/com/example/appmobile/RegisterActivity.java
@@ -14,19 +14,19 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        Button register = findViewById(R.id.register);
 
-        register.setOnClickListener(new View.OnClickListener() {
+
+      /*  register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                if (true){ //connexion ok
+               if (true){ //connexion ok
                     SaveSharedPreference.setUserName(getBaseContext(), "testuser");
                 }
                 Intent intent=new Intent(getBaseContext(),MainActivity.class);
                 intent.putExtra("tabToSelect", 1);
                 startActivity(intent);
             }
-        });
+        });*/
     }
 }
