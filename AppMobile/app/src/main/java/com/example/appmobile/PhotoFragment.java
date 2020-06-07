1838949c50dc11a4cfb2a422efce594938d6fc29
Soizik <trehou.thoms@outlook.fr>
2020-06-07 23:06:10 +0200

    Color Background

--- AppMobile/app/src/main/java/com/example/appmobile/PhotoFragment.java
+++ AppMobile/app/src/main/java/com/example/appmobile/PhotoFragment.java
@@ -65,10 +65,10 @@ public class PhotoFragment extends Fragment {
 
         View inflatedView = null;
         if(SaveSharedPreference.getUserName(getActivity().getBaseContext()).length() == 0) {
-            inflatedView = inflater.inflate(R.layout.fragment_photo, container, false);
+            inflatedView = inflater.inflate(R.layout.fragment_photo2, container, false);
         }
         else {
-            inflatedView = inflater.inflate(R.layout.fragment_photo2, container, false);
+            inflatedView = inflater.inflate(R.layout.fragment_photo, container, false);
 
 
             validerPhoto = inflatedView.findViewById(R.id.btn_valider);
