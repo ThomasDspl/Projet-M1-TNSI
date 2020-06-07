2fd0de062c1545cb28589ea9a59d51a068f2d22e
Soizik <trehou.thoms@outlook.fr>
2020-06-07 16:25:51 +0200

    Ajout photo V2

--- AppMobile/app/src/main/java/com/example/appmobile/ClassementFragment.java
+++ AppMobile/app/src/main/java/com/example/appmobile/ClassementFragment.java
@@ -1,9 +1,12 @@
 package com.example.appmobile;
 
+import android.database.MatrixCursor;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,6 +16,7 @@ public class ClassementFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
         return inflater.inflate(R.layout.fragment_classement,container, false);
     }
 }
