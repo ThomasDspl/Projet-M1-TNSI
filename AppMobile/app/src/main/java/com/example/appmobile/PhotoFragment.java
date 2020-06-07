6ed694db3520c685a6991a9b679aaefedec8e0e3
ThomasDspl <thomas.displaire@outlook.fr>
2020-06-07 16:07:03 +0200

    Add photo picker

--- AppMobile/app/src/main/java/com/example/appmobile/PhotoFragment.java
+++ AppMobile/app/src/main/java/com/example/appmobile/PhotoFragment.java
@@ -1,19 +1,29 @@
 package com.example.appmobile;
 
+import android.Manifest;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
@@ -21,26 +31,82 @@ import androidx.fragment.app.FragmentTransaction;
 
 import com.example.appmobile.ui.main.PageViewModel;
 import com.example.appmobile.ui.main.SectionsPagerAdapter;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+import static android.app.Activity.RESULT_OK;
 
 public class PhotoFragment extends Fragment {
+
+    FloatingActionButton cherchePhoto;
+    ImageView viewPhoto;
+    int RESULT_LOAD_IMAGE;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        final View inflatedView = inflater.inflate(R.layout.fragment_photo, container, false);
-        
-        /*
-        if(SaveSharedPreference.getUserName(getActivity().getBaseContext()).length() != 0)
-        {
-            ViewGroup layout = (ViewGroup) connexion.getParent();
-            if(null!=layout) //for safety only  as you are doing onClick
-                layout.removeView(connexion);
-                layout.removeView(inscription);
-        }*/
+
+        View inflatedView = null;
+        if(SaveSharedPreference.getUserName(getActivity().getBaseContext()).length() != 0) {
+            inflatedView = inflater.inflate(R.layout.fragment_photo2, container, false);
+        }
+        else{
+            inflatedView = inflater.inflate(R.layout.fragment_photo, container, false);
+        }
+
+        cherchePhoto = inflatedView.findViewById(R.id.btn_cherchePhoto);
+        viewPhoto = inflatedView.findViewById(R.id.imageView_photo);
+
+        cherchePhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
+                {
+                    requestPermissions(
+                            new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                            2000);
+                }
+                else {
+                    try{
+                        Intent i = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                        startActivityForResult(i, RESULT_LOAD_IMAGE);
+                    }
+                    catch(Exception exp){
+                        Log.i("Error",exp.toString());
+                    }
+                }
+            }
+        });
 
 
         return inflatedView;
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //super method removed
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+                Log.d("wesh", "onActivityResult: ");
+
+                Uri returnUri = data.getData();
+                Bitmap bitmapImage = null;
+                try {
+                    bitmapImage = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), returnUri);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                viewPhoto.setImageBitmap(bitmapImage);
+
+        }
+    }
+
+
+
 
 
 
